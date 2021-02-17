80a449b4cf4bed03bbb87c961fd99bcea3eaed04
Igor <klyuzhevigor>
2021-02-17 18:46:13 +0300

    Corrected output of logs

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -11,37 +11,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        NSLog("View did load : \(#function)")
+        NSLog("\nView did load : \(#function)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        NSLog("View will appear : \(#function)")
+        NSLog("\nView will appear : \(#function)")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        NSLog("View will layout subviews : \(#function)")
+        NSLog("\nView will layout subviews : \(#function)")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        NSLog("View did layout subviews : \(#function)")
+        NSLog("\nView did layout subviews : \(#function)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        NSLog("View did appear : \(#function)")
+        NSLog("\nView did appear : \(#function)")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        NSLog("View will disappear : \(#function)")
+        NSLog("\nView will disappear : \(#function)")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        NSLog("View did disappear : \(#function)")
+        NSLog("\nView did disappear : \(#function)")
     }
 }
 
