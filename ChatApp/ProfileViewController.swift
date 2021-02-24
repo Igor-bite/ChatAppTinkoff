ed65584d9fe23bc67545fc4a21a4057511c02c47
Igor <klyuzhevigor>
2021-02-25 01:13:18 +0300

    Made a design on a storyboard and defined a function for user image tap

--- ChatApp/ProfileViewController.swift
+++ ChatApp/ProfileViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var editButtonView: UIView?
+    @IBOutlet weak var userNameLabel: UILabel?
+    @IBOutlet weak var userDetailsLabel: UILabel?
+    @IBOutlet weak var userImageView: UIView?
+    @IBOutlet weak var userImageLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        editButtonView?.layer.cornerRadius = 14
+        
+        userImageView?.layer.cornerRadius = 120
+        
+        let userImageRec = UITapGestureRecognizer(target: self, action: #selector(userImageTapped))
+        userImageView?.addGestureRecognizer(userImageRec)
+    }
+    
+    @objc func userImageTapped(_ sender: UITapGestureRecognizer) {
         
     }
 }
+
+
