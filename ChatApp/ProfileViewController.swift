d0ed435d7afbb242ee9754df78c2c977488485c1
Igor <klyuzhevigor>
2021-02-24 23:15:51 +0300

    Created new file for ProfileViewController

---
+++ ChatApp/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  ChatApp
+//
+//  Created by Игорь Клюжев on 24.02.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
