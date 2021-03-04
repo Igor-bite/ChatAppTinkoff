f66f71b9d5fb796735523930ff14d692fd56e3fd
Igor <klyuzhevigor>
2021-03-04 20:14:47 +0300

    added ConversationsListViewController and connected with storyboard

---
+++ ChatApp/ConversationsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConversationsListViewController.swift
+//  ChatApp
+//
+//  Created by Игорь Клюжев on 04.03.2021.
+//
+
+import UIKit
+
+class ConversationsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
