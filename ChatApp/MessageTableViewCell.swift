e34a4520ef6f2957461c514e6c7786756a7468ef
Igor <klyuzhevigor>
2021-03-05 03:00:37 +0300

    corrected message bubbles

--- ChatApp/MessageTableViewCell.swift
+++ ChatApp/MessageTableViewCell.swift
@@ -18,24 +18,20 @@ class MessageTableViewCell: UITableViewCell, MessageCellConfiguration {
     
     func configure(text: String, isFromMe: Bool) {
         messageLabel?.text = text
-//        guard let view = messageView else { return }
-//        let constraintMessageViewLeading: NSLayoutConstraint
+        messageLabel?.textColor = .white
         if isFromMe {
             messageView?.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
-//            constraintMessageViewLeading = NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1.0, constant: 5)
         } else {
             messageView?.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
-//            constraintMessageViewLeading = NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1.0, constant: 5)
         }
         messageView?.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([constraintMessageViewLeading])
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         messageView?.backgroundColor = UIColor(red: 0, green: 0, blue: 150/255, alpha: 0.7)
         
-        messageView?.layer.cornerRadius = 20
+        messageView?.layer.cornerRadius = 15
         
     }
 }
