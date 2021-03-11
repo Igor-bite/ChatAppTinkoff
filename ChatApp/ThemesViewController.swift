dc346da5c9ff96982ac95f847cc06f1cf7fc79e0
Igor <klyuzhevigor>
2021-03-11 13:10:47 +0300

    Added enum for themes and cancel button

--- ChatApp/ThemesViewController.swift
+++ ChatApp/ThemesViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
+enum Theme: String {
+    case classic = "classic"
+    case day = "day"
+    case night = "night"
+}
+
 class ThemesViewController: UIViewController {
+    let lastTheme: Theme = Theme(rawValue: UserDefaults.standard.string(forKey: "theme") ?? "classic") ?? Theme.classic
+    var currentTheme: Theme = Theme(rawValue: UserDefaults.standard.string(forKey: "theme") ?? "classic") ?? Theme.classic
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Settings"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(restoreSettings))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func restoreSettings() {
+        changeTheme(to: lastTheme)
+        navigationController?.popViewController(animated: true)
     }
-    */
-
+    
+    func saveSettings() {
+        
+    }
+    
+    func changeTheme(to newTheme: Theme) {
+        currentTheme = newTheme
+        switch newTheme {
+        case .classic:
+            print("classic theme chosen")
+        case .day:
+            print("day theme chosen")
+        case .night:
+            print("night theme chosen")
+        }
+        saveSettings()
+    }
 }
