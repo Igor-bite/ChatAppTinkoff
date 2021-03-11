f99c0cf96ba96be9b4c3e642a384d26f38359d2b
Igor <klyuzhevigor>
2021-03-11 21:24:07 +0300

    Added Buttons to change theme

--- ChatApp/ThemesViewController.swift
+++ ChatApp/ThemesViewController.swift
@@ -15,33 +15,55 @@ enum Theme: String {
 
 let themeKeyIdentifier = "theme"
 
+let userDefaultsManager = UserDefaults.standard
+
 func getSavedTheme() -> Theme {
-    return Theme(rawValue: UserDefaults.standard.string(forKey: themeKeyIdentifier) ?? "classic") ?? Theme.classic
+    return Theme(rawValue: userDefaultsManager.string(forKey: themeKeyIdentifier) ?? "classic") ?? Theme.classic
 }
 
 class ThemesViewController: UIViewController {
     let lastTheme: Theme = getSavedTheme()
     var currentTheme: Theme = getSavedTheme()
+    
+    @IBOutlet weak var classicThemeView: UIView?
+    @IBOutlet weak var dayThemeView: UIView?
+    @IBOutlet weak var nightThemeView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Settings"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(restoreSettings))
+        
+        makeRoundCorners(for: classicThemeView)
+        makeRoundCorners(for: dayThemeView)
+        makeRoundCorners(for: nightThemeView)
+        
+        let gestureRecognizerClassic = UITapGestureRecognizer(target: self, action: #selector(changeToClassic))
+        classicThemeView?.addGestureRecognizer(gestureRecognizerClassic)
+        
+        let gestureRecognizerDay = UITapGestureRecognizer(target: self, action: #selector(changeToDay))
+        dayThemeView?.addGestureRecognizer(gestureRecognizerDay)
+        
+        let gestureRecognizerNight = UITapGestureRecognizer(target: self, action: #selector(changeToNight))
+        nightThemeView?.addGestureRecognizer(gestureRecognizerNight)
+    }
+    
+    func makeRoundCorners(for view: UIView?) {
+        if let subviews = view?.subviews {
+            for subview in subviews {
+                if subview.tag == 1 {
+                    subview.layer.cornerRadius = 14
+                    for sub in subview.subviews {
+                        sub.layer.cornerRadius = 10
+                    }
+                }
+            }
+        }
     }
     
     @objc func restoreSettings() {
-        changeTheme(to: lastTheme)
-        navigationController?.popViewController(animated: true)
-    }
-    
-    func saveSettings() {
-        
-    }
-    
-    func changeTheme(to newTheme: Theme) {
-        currentTheme = newTheme
-        switch newTheme {
+        switch lastTheme {
         case .classic:
             print("classic theme chosen")
         case .day:
@@ -49,6 +71,22 @@ class ThemesViewController: UIViewController {
         case .night:
             print("night theme chosen")
         }
-        saveSettings()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func saveSettings() {
+        userDefaultsManager.setValue(currentTheme, forKey: themeKeyIdentifier)
+    }
+    
+    @objc func changeToClassic() {
+        print("classic theme chosen")
+    }
+    
+    @objc func changeToDay() {
+        print("day theme chosen")
+    }
+    
+    @objc func changeToNight() {
+        print("night theme chosen")
     }
 }
