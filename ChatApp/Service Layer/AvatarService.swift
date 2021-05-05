78e440324d3d912179b49df4be1751229fb8a331
Igor <klyuzhevigor@gmail.com>
2021-05-05 12:33:23 +0300

    fix: fixed class keyword for protocol for Xcode 12.5

--- ChatApp/Service Layer/AvatarService.swift
+++ ChatApp/Service Layer/AvatarService.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-protocol AvatarService: class {
+protocol AvatarService: Any {
     var imageLoader: ImageLoader { get }
     func getDataSource() -> UICollectionViewDataSource
     func getPreviewImage(for indexPath: IndexPath) -> UIImage?
